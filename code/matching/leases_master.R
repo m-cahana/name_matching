cc7c6808d34325037dfecb9383a69a76e0ee914c
Michael Cahana <cahanamichael@gmail.com>
2019-02-19 12:21:32 -0600

    adding common words, separating leases and modeled processes

---
+++ code/matching/leases_master.R
@@ -0,0 +1,80 @@
+# Created by Michael Cahana in mid Feb. 2018
+# Determines name matches given name and address inputs
+
+#===========
+# inputs: 
+#===========
+# landtrac_tx
+# coded_addresses.csv
+
+
+#===========
+# needed libraries
+#===========
+library(tidyverse)
+library(fst)
+library(stringi)
+library(readxl)
+
+#===========
+# standard setup
+#===========
+root <- getwd()
+while(basename(root) != "name_matching") {
+  root <- dirname(root)
+}
+source(file.path(root, "data.R"))
+
+#===========
+# functions
+#===========
+
+source(file.path(root, 'code', 'matching', 'match_names.R'))
+source(file.path(root, 'code', 'matching', 'match_addresses.R'))
+source(file.path(root, 'code', 'matching', 'filter_names.R'))
+
+#===========
+# data read in
+#===========
+
+leases <- readRDS(file.path(rdir, 'leases', 'landtrac_tx.Rds'))
+already_coded_addresses <- read_csv(file.path(ddir, 'coded_addresses.csv'))
+
+#===========
+# match names within modeled
+#===========
+
+df <- 
+    leases %>% 
+    count(grnte_al) %>% 
+    rename(name = grnte_al)
+
+output_file <- file.path(ddir, 'matches', 'leases_name_matches.csv')
+
+match_names(df, output_file)
+
+#===========
+# match addresses within modeled
+#===========
+
+already_coded_addresses <-  pull(already_coded_addresses, address)
+
+df <- 
+	leases %>% 
+	count(grnte_ad) %>% 
+	rename(address = grnte_ad)
+
+output_file <- file.path(ddir, 'matches', 'leases_address_matches.csv')
+
+match_addresses(df, already_coded_addresses, output_file)
+
+#===========
+# filter down name match list
+#===========
+
+name_matches <- read_csv(file.path(ddir, 'matches', 'leases_name_matches.csv'))
+address_matches <- read_csv(file.path(ddir, 'matches', 
+	'leases_address_matches.csv'))
+output_file <- file.path(vdir, 'leases_name_matches.csv')
+
+filter_names(name_matches, address_matches, output_file)
