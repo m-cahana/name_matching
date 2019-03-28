d5efbaa1884954c7e9d1a71cf7534410cd80b330
Michael Cahana <cahanamichael@gmail.com>
2019-03-28 16:36:18 -0500

    set up bare-bones makefile

---
+++ code/matching/match_leases_addresses.R
@@ -0,0 +1,56 @@
+# Created by Michael Cahana in mid Feb. 2018
+# Determines address matches for leases
+
+#===========
+# inputs: 
+#===========
+# landtrac_tx
+# coded_addresses.csv
+
+#===========
+# needed libraries
+#===========
+library(tidyverse)
+library(sf)
+
+#===========
+# standard setup
+#===========
+root <- getwd()
+while(basename(root) != "name_matching") {
+  root <- dirname(root)
+}
+source(file.path(root, "data.R"))
+
+#===========
+# functions
+#===========
+
+source(file.path(root, 'code', 'matching', 'functions', 'match_addresses.R'))
+
+#===========
+# data read in
+#===========
+
+leases <- 
+	readRDS(file.path(rdir, 'leases', 'landtrac_tx.Rds')) %>% 
+	st_set_geometry(NULL) %>% 
+	as_tibble()
+already_coded_addresses <- read_csv(file.path(ddir, 'address_backups', 
+	'coded_addresses.csv')) 
+
+#===========
+# match addresses 
+#===========
+
+already_coded_addresses <-  pull(already_coded_addresses, address)
+
+df <- 
+	leases %>% 
+	rename(address = grnte_ad, name = grnte_al) %>% 
+	select(name, address) 
+
+output_file <- file.path(ddir, 'matches', 'addresses', 
+	'leases_address_matches.csv')
+
+match_addresses(df, already_coded_addresses, output_file)
---
+++ code/package_installation.R
@@ -0,0 +1,19 @@
+# Install necessary packages
+# data shaping packages: tidyverse, fst, stringi, tictoc, stringdist, text2vec, 
+# readxl, fuzzyjoin, data.table, sf, googleway, igraph, openxlsx, lubridate, 
+# e1071, rpart, randomForest
+
+packages <- c('tidyverse', 'fst', 'stringi', 'tictoc', 'stringdist', 'text2vec', 
+	'readxl', 'fuzzyjoin', 'data.table', 'sf', 'googleway', 'igraph', 
+	'openxlsx', 'lubridate', 'e1071', 'rpart', 'randomForest')
+
+check_install <- function(pkg){
+	if(!(pkg %in% installed.packages())){
+		install.packages(pkg, repos="http://cran.rstudio.com/")
+		return(paste(pkg, " is now installed"))
+	} else {
+		return(paste(pkg, " is already installed"))
+	}
+}
+
+lapply(packages, check_install)
