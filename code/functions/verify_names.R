b4c5cd2e05d4a55c094ab5c338fba3fcc276b026
Michael Cahana <cahanamichael@gmail.com>
2019-03-29 09:47:04 -0500

    don't overwrite human reviews if they exist'

--- code/functions/verify_names.R
+++ code/functions/verify_names.R
@@ -27,14 +27,22 @@ verify_names <- function(name_matches, address_matches, lease_count,
 		name_matches %>% 
 		left_join(address_matches, by=c('name','match')) %>% 
 	    mutate(keep = if_else(!is.na(address), 1, as.double(NA))) %>% 
-	    left_join(count, by='name') %>% 
-	    left_join(count, by=c('match' = 'name')) %>% 
+	    left_join(lease_count, by='name') %>% 
+	    left_join(lease_count, by=c('match' = 'name')) %>% 
 	    mutate(n.x = ifelse(duplicated(name), 0, n.x)) %>% 
 	    mutate(n.y = ifelse(duplicated(match), 0, n.y)) %>% 
 	    mutate(n.y = ifelse(match %in% .$name, 0, n.y)) %>% 
 	    mutate(n = n.x + n.y) %>% 
 	    arrange(desc(n)) %>% 
 	    mutate(pct_coverage = cumsum(n)/sum(n)) 
+
+	if (file.exists(output_file)) {
+		pre_existing_name_matches <- read_csv(output_file)
+		name_matches <- 
+			pre_existing_name_matches %>% 
+			bind_rows(name_matches) %>% 
+			distinct(name, match, .keep_all = T)
+	}
 
 	write_csv(name_matches, output_file)
 }
