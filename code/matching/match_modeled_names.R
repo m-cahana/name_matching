ec855b210bfefc3f7a5673676a1658b0bde70f65
Michael Cahana <cahanamichael@gmail.com>
2019-04-09 17:03:04 -0500

    use common_oper_name to match names for modeled data
    rather than curr_oper_name

--- code/matching/match_modeled_names.R
+++ code/matching/match_modeled_names.R
@@ -36,7 +36,8 @@ source(file.path(root, 'code', 'functions', 'match_names.R'))
 #===========
 
 desc <- read_fst(file.path(rdir, 'pden_desc-2018-09-26.fst'), 
-    columns = c('api_no', 'curr_oper_id', 'curr_oper_no', 'curr_oper_name')) %>% 
+    columns = c('api_no', 'curr_oper_id', 'curr_oper_no', 'curr_oper_name', 
+        'common_oper_name')) %>% 
         mutate(api_no = str_replace_all(api_no, '-', '') %>% 
         stri_pad_right(14, 0)) 
 modeled <- readRDS(file.path(rdir, 'modeled_prices.Rds')) 
@@ -53,8 +54,8 @@ df <-
     inner_join(desc, by='api_no') %>% 
     left_join(cleaned_300, by='curr_oper_name') %>% 
     mutate(replacement = 
-        if_else(is.na(replacement), curr_oper_name, replacement)) %>% 
-    select(-curr_oper_name) %>% 
+        if_else(is.na(replacement), common_oper_name, replacement)) %>% 
+    select(-common_oper_name) %>% 
     rename(name = replacement)
 output_file <- file.path(ddir, 'matches', 'names', 'modeled_name_matches.csv')
 
