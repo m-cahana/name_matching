a50550a70f166ed269287b8528932704c5040bed
Michael Cahana <cahanamichael@gmail.com>
2019-04-09 14:45:10 -0500

    incorporate new coded sample into grf training/testing

--- code/analysis/grf_examples.R
+++ code/analysis/grf_examples.R
@@ -23,14 +23,17 @@ library(grf)
 # Michael Cahana's canned and pre-labeled pairs
 #===========
 df <- 
-  read_csv(file.path(dropbox, 'archive', 'lease_match_sample.csv')) %>%
-  filter(!is.na(keep))
+  c(paste(file.path(dropbox, 'archive'), 
+    c('lease_match_sample.csv', 'new_lease_sample.csv'), sep='/')) %>% 
+  map_df(read_csv) %>% 
+  filter(!is.na(keep)) %>% 
+  distinct(name, match, .keep_all = T)
 
 #===========
 # example grf use
 #===========
 rf <-
-  paste("shared_words", "cosine_similarity", "dist_score", sep = "+") %>%
+  paste("shared_words", "cosine_similarity", "jw_distance", sep = "+") %>%
   paste("keep", ., sep = "~") %>%
   as.formula %>%
   regression_forest2(df)
@@ -41,7 +44,7 @@ fig <-
   as_tibble %>%
   bind_cols(df) %>% 
   ggplot(aes(x = predictions, fill = as.factor(keep))) + 
-  geom_histogram(position = 'dodge') + 
-  scale_x_continuous(breaks = seq(.0,.8,.1)) + 
+  geom_histogram(position = 'dodge', binwidth = .05) + 
+  scale_x_continuous(breaks = seq(.0,1,.1)) + 
   scale_fill_discrete(name = "keep")
 
