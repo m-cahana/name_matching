6b7d286c43a1e53ed4b7b93b54a78cbc9a4e638c
Michael Cahana <cahanamichael@gmail.com>
2019-04-16 15:27:07 -0500

    generate RF predictions for all name_matches
    add predict2() function to random_forest_utils to apply RF onto a non-training dataset

--- code/analysis/grf_examples.R
+++ code/analysis/grf_examples.R
@@ -20,28 +20,48 @@ library(tidyverse)
 library(grf)
 
 #===========
-# Michael Cahana's canned and pre-labeled pairs
+# data read-in
 #===========
+
+# pre-labeled pairs for training
 df <- 
-  read_csv(file.path(dropbox, 'archive', 'lease_match_sample.csv')) %>%
-  filter(!is.na(keep))
+  c(paste(file.path(dropbox, 'archive'), 
+    c('lease_match_sample.csv', 'new_lease_sample.csv'), sep='/')) %>% 
+  map_df(read_csv) %>% 
+  filter(!is.na(keep)) %>% 
+  distinct(name, match, .keep_all = T) 
+
+# all name matches
+name_matches <- read_csv(file.path(vdir, 'leases_matches.csv'))
 
 #===========
 # example grf use
 #===========
-rf <-
-  paste("shared_words", "cosine_similarity", "dist_score", sep = "+") %>%
+func <-  
+  paste("shared_words", "cosine_similarity", "jw_distance", sep = "+") %>%
   paste("keep", ., sep = "~") %>%
-  as.formula %>%
+  as.formula()
+
+rf <-
+  function %>%
   regression_forest2(df)
 
-fig <-
+sample_fig <-
   rf %>%
   predict %>%
   as_tibble %>%
   bind_cols(df) %>% 
   ggplot(aes(x = predictions, fill = as.factor(keep))) + 
   geom_histogram(position = 'dodge') + 
-  scale_x_continuous(breaks = seq(.0,.8,.1)) + 
+  scale_x_continuous(breaks = seq(.0,1,.1)) + 
   scale_fill_discrete(name = "keep")
 
+name_matches_fig <-
+  rf %>%
+  predict2(func, name_matches) %>%
+  as_tibble() %>%
+  bind_cols(name_matches) %>% 
+  ggplot(aes(x = predictions, fill = as.factor(keep))) + 
+  geom_histogram(position = 'dodge') + 
+  scale_x_continuous(breaks = seq(.0,1,.1)) + 
+  scale_fill_discrete(name = "keep")
